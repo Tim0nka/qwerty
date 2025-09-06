d9e792e2c7b3b10d73e38333c13129bb012e34f5
Tim0nka <timonvaismann@tutamail.com>
2025-09-06 08:49:44 +0300

    Add files via upload

--- aio.py
+++ aio.py
@@ -1,13 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiogram.utils.keyboard import KeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import Command
+from aiogram import F
 
 from secret import secrets
 
 
 # ==== НАСТРОЙКИ ====
-TELEGRAM_TOKEN = secrets["BOT_API_TOKEN"]
+TELEGRAM_TOKEN = secrets['BOT_API_TOKEN']
 
 # Создаём экземпляры бота и диспетчера
 bot = Bot(token=TELEGRAM_TOKEN)
@@ -16,17 +18,40 @@ dp = Dispatcher()
 # Обработчик команды /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-
-    kb = [
-        [types.KeyboardButton(text="Кнопка 1")],
-        [types.KeyboardButton(text="Кнопка 2")]
-    ]
-
     await message.answer("Привет! Я простой бот на aiogram 🤖")
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
-    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Сказать привет 👋", callback_data="hello")],
+        [InlineKeyboardButton(text="Показать смайлик 😄", callback_data="show_smile")],
+        [InlineKeyboardButton(text="Показать видео 🎬", callback_data="show_video")],
+        [InlineKeyboardButton(text="Перейти на сайт 🌐", url="https://example.com")]
+    ])
+    await message.answer("Добро пожаловать! Выберите действие:", reply_markup=keyboard)
 
+@dp.callback_query(F.data == "show_smile")
+async def send_smile(callback: types.CallbackQuery):
+    await callback.answer()  # ← обязательно!
+    await callback.message.answer_photo(
+        photo="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Smiley_face.png/600px-Smiley_face.png",
+        caption="Вот твой смайлик! 😄"
+    )
 
+@dp.callback_query(F.data == "show_image")
+async def send_image(callback: types.CallbackQuery):
+    await callback.answer()
+    await callback.message.answer_photo(
+        photo="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Smiley_face.png/600px-Smiley_face.png",
+        caption="Вот твой смайлик! 😄"
+    )
+
+@dp.callback_query(F.data == "show_video")
+async def send_video(callback: types.CallbackQuery):
+    await callback.answer()
+    await callback.message.answer_video(
+        video="https://filesamples.com/samples/video/mp4/sample_640x360.mp4",
+        caption="Вот видео в формате MP4 🎬"
+    )
+
+# Обработчик любого текстового сообщения
 @dp.message()
 async def echo(message: types.Message):
     await message.answer(f"Ты написал: {message.text}")
